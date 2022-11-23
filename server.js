8a969714bcc99d70482b927bc4f45acb56393d6a
Avishrant Sharma <avishrants@gmail.com>
2022-11-23 10:50:23 +0530

    Add support for swagger-ui - Auto API documentation using swagger-autogen
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
 
 require("dotenv/config");
 const PORT = process.env.PORT || 5000;
@@ -21,6 +23,9 @@ mongoose
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(passport.initialize());
+
+    // Routes
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
     app.use("/", require("./routes/index"));
 
     app.listen(PORT, () => {
